ea783c026153f15319a2aff076533803a6bfbc8b
Fahime <fahim.mohammad.siam@gmail.com>
2023-09-23 14:27:27 +0600

    Header component designed

---
+++ src/components/AppliedJobs/Appliedjobs.jsx
@@ -0,0 +1,10 @@
+// @ts-nocheck
+const Appliedjobs = () => {
+  return (
+    <div>
+      <h2>Jobs i applied</h2>
+    </div>
+  );
+};
+
+export default Appliedjobs;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root/Root";
 import Home from "./components/Home/Home";
+import Appliedjobs from "./components/AppliedJobs/Appliedjobs";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/applied",
+        element: <Appliedjobs></Appliedjobs>,
       },
     ],
   },
