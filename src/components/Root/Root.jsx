761351fb068dd3bcd0fd24a322c89cd11fdf4113
Fahime <fahim.mohammad.siam@gmail.com>
2023-09-23 14:02:07 +0600

    Root, Home and footer component made and react router set up

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+
+const Root = () => {
+  return (
+    <div>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
